486c9988ba41ac0f090bd2b296fb89bb977fbcea
Gabriel Luna <gabriellunajob@gmail.com>
2026-01-09 13:36:19 -0300

    valor id corrigido

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,28 +2,28 @@ import { EducationItem } from "./types";
 
 export const education: EducationItem[] = [
   {
-    id: 1, // Adicionei ID para controle
+    id: "1", 
     course: "edu.umc.course",
     institution: "edu.umc.inst",
     period: "edu.umc.period",
     description: "edu.umc.desc"
   },
   {
-    id: 2,
+    id: "2",
     course: "edu.obc.course",
     institution: "edu.obc.inst",
     period: "edu.obc.period",
     description: "edu.obc.desc"
   },
   {
-    id: 3,
+    id: "3",
     course: "edu.origamid.course",
     institution: "edu.origamid.inst",
     period: "edu.origamid.period",
     description: "edu.origamid.desc"
   },
   {
-    id: 4,
+    id: "4",
     course: "edu.impacta.course",
     institution: "edu.impacta.inst",
     period: "edu.impacta.period",
